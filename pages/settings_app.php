e462f57b9f5aec159f9de42716b9eb8d05d7005d
boddaxp <bodemix@gmail.com>
2017-12-22 20:20:41 +0200

    Themes

--- pages/settings_app.php
+++ pages/settings_app.php
@@ -3,7 +3,23 @@
 			<div class="row">
 				<div class="col-lg-12">
 					<h1 class="page-header">Application Settings</h1>
-					<h3>Defaults, Themes..</h3>
+				</div>
+				<div>
+					<h3>Themes</h3>
+					<div class="row">
+						<div class="themePreview col-lg-4 col-sm-6 col-xs-12">
+							<img src="dist/css/preview/theme1.png" onclick="changeTheme('style1.css')">
+							<p>Blue Theme (Main)</p>
+						</div>
+						<div class="themePreview col-lg-4 col-sm-6 col-xs-12" onclick="changeTheme('style2.css')">
+							<img src="dist/css/preview/theme2.png">
+							<p>Silver Theme</p>
+						</div>	
+						<div class="themePreview col-lg-4 col-sm-6 col-xs-12" onclick="changeTheme('style3.css')">
+							<img src="dist/css/preview/theme3.png">
+							<p>Lime Theme</p>
+						</div>	
+					</div>
 				</div>
 				<!-- /.col-lg-12 -->
 			</div>
