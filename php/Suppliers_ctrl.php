afe6d34f08b15e7e053728023759c2a155d3ddd2
spearkiller0 <abdohamdymetwaly@gmail.com>
2017-12-22 19:07:01 +0200

    MONA MARDY:  ADDED QUERY FOR SUPPLIERS AND MEDICINES

---
+++ php/Suppliers_ctrl.php
@@ -0,0 +1,26 @@
+<?
+ob_start();
+session_start();
+include_once("MySQLi.php");
+header('Content-Type: application/json');
+$db->query
+	("
+		INSERT INTO suppliers
+			(
+				Supplier_ID,
+				Name,
+				Phone,
+				Location,
+				Email
+			)
+		VALUES 
+			(
+				NULL,
+				'".$db->escape($_POST['Name'])."',
+				'".$db->escape($_POST['Phone'])."',
+				'".$db->escape($_POST['Loc'])."',
+				'".$db->escape($_POST['mailh'])."'
+			)
+	");
+
+?>
