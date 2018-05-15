e1357c5e0cdbab6b562c32714bb3947fb523bd89
spearkiller0 <abdohamdymetwaly@gmail.com>
2018-05-15 15:41:36 +0200

    notification javascript editing

--- pages/notifications.php
+++ pages/notifications.php
@@ -1,3 +1,47 @@
+<script>
+window.onload = function(){
+	Send("./php/Notification_table.php","GET",function(data){
+
+		$.each(data.Expired,function(index,row){
+			
+
+
+		});
+
+
+
+		$.each(data.Epire_Soon,function(index,row){
+
+
+
+		});
+
+
+
+		$.each(data.Out_of_stock,function(index,row){
+
+
+
+		});
+
+
+		
+
+
+
+
+
+
+		$('#dataTables-example').DataTable({
+            responsive: true
+        });
+	});	
+}
+	
+</script>
+
+
+
 <div id="page-wrapper">
 	<div class="container-fluid">
 		<div class="row">
